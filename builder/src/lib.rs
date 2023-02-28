2880199106b61c3c5a566392ca4c2f6c4941b1b2
DatNguyen <dat.n@sharering.network>
2023-02-28 21:08:21 +0700

    builder: test6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -19,24 +19,48 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let attr_opts = fields.iter().map(|e| {
         let ident = &e.ident;
         let ty = &e.ty;
-        quote! {
-            #ident: Option<#ty>,
+        if get_inner_option(ty).is_none() {
+            quote! {
+                #ident: Option<#ty>,
+            }
+        } else {
+            quote! {
+                #ident: #ty,
+            }
         }
     });
     let setters = fields.iter().map(|e| {
         let ident = &e.ident;
         let ty = &e.ty;
-        quote! {
-            fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                self.#ident = Some(#ident);
-                self
+        let inner = get_inner_option(ty);
+        if let Some(inner) = inner {
+            quote! {
+                fn #ident(&mut self, #ident: #inner) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
             }
         }
     });
     let attr_opt_errs = fields.iter().map(|e| {
         let ident = &e.ident;
-        quote! {
-            #ident: self.#ident.clone().ok_or("missing attribute XXXX")?,
+        let ty = &e.ty;
+        let inner = get_inner_option(ty);
+        if inner.is_some() {
+            quote! {
+                #ident: self.#ident.clone(),
+            }
+        } else {
+            quote! {
+                #ident: self.#ident.clone().ok_or("missing attribute XXXX")?,
+            }
         }
     });
     let build_method = quote! {
@@ -74,4 +98,30 @@ fn get_all_fields(data: syn::Data) -> syn::punctuated::Punctuated<Field, Token![
         unimplemented!()
     };
     named
+}
+
+fn get_inner_option(ty: &syn::Type) -> Option<syn::Type> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = ty
+    {
+        let v: Vec<_> = segments.iter().map(|e| e.ident.to_string()).collect();
+        if v.last().unwrap() == "Option" {
+            // get first element of
+            if let syn::PathSegment {
+                arguments:
+                    syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                        args, ..
+                    }),
+                ..
+            } = segments.iter().last().unwrap()
+            {
+                if let syn::GenericArgument::Type(tp) = args.first().unwrap() {
+                    return Some(tp.clone());
+                }
+            }
+        }
+    }
+    None
 }
